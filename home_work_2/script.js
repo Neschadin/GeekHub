9b865f45bf6db50459094d67a876ad2a7be94029
Neschadin <abudarovich@gmail.com>
2022-10-26 00:49:27 +0300

    Update script.js
    HW2 done!

--- home_work_2/script.js
+++ home_work_2/script.js
@@ -1,21 +1,17 @@
 // Створити функцію multiple() яка може приймати не обмежену кількість аргументів, та перемножує їх
-function multiply(...args) {
-    return args.reduce((acc, cur) => acc * cur);
-}
+let multiply = (...args) => args.reduce((acc, cur) => acc * cur);
 
-console.log(`Створити функцію multiple()`);
-console.log(multiply(2, 3, 4));
+multiply(2, 3, 4);
 
 
-// Створити фунцію reverseString яка приймає 1 аргумент будь-якого типу, і розвертає його. Наприклад: ‘test’ -> `tset`, undefined -> ‘denifednu’
+// Створити фунцію reverseString яка приймає 1 аргумент будь-якого типу, і розвертає його. Наприклад: ‘test’ -> 'tset', undefined -> ‘denifednu’
 function reverseString(arg) {
-    if (typeof arg != `string`) arg = typeof arg;
+    if (typeof arg != 'string') arg = typeof arg;
     return arg.split("").reverse().join("");
 }
 
-console.log(`\nСтворити фунцію reverseString`);
-console.log(reverseString(null));
-console.log(reverseString(`GeekHub`));
+reverseString(null);
+reverseString('GeekHub');
 
 
 // Створити фунцію вгадати число. Умови:
@@ -25,21 +21,21 @@ console.log(reverseString(`GeekHub`));
 // Далі функція генерує рандомне число від 1 до 10 і якщо задане число правильне повертає стрінгу ‘You Win!’, 
 // якщо не правильно ‘You are lose, your number is 8, the random number is 5’
 function guessing(num) {
-    if (!isFinite(num)) return `Please provide a valid number`;
-    if (num < 1 || num > 10) return `Please provide number in range 1 - 10`;
+    if (!isFinite(num)) return 'Please provide a valid number';
+    if (num < 1 || num > 10) return 'Please provide number in range 1 - 10';
     let randomNum = Math.floor(Math.random() * 10) + 1;
     return num === randomNum ?
-        `You Win!` :
+        'You Win!' :
         `You are lose, your number is ${num}, the random number is ${randomNum}`;
 }
 
-console.log(`\nСтворити фунцію вгадати число`);
-console.log(guessing(`addf`));
-console.log(guessing(NaN));
-console.log(guessing(Infinity));
-console.log(guessing(-2));
-console.log(guessing(5));
-console.log(guessing(10));
+guessing('addf');
+guessing(NaN);
+guessing(Infinity);
+guessing(-2);
+guessing(5);
+guessing(10);
+
 
 // Є масив чисел (додатних, відʼємних, і впереміш). Потрібно знайти min, max, sum. Не можна використовувати методи масивів або обʼєкту Math, а тільки цикли for і while. Приклади масивів:
 // [3,0,-5,1,44,-12,3,0,0,1,2,-3,-3,2,1,4,-2-3-1]
@@ -60,14 +56,11 @@ function minMaxSum(arr) {
     return `min = ${min}, max = ${max}, sum = ${sum}`;
 }
 
-console.log(`\nзнайти min, max, sum`);
-console.log(
-    minMaxSum([3, 0, -5, 1, 44, -12, 3, 0, 0, 1, 2, -3, -3, 2, 1, 4, -2 - 3 - 1])
-);
-console.log(minMaxSum([-1, -8, -2]));
-console.log(minMaxSum([1, 7, 3]));
-console.log(minMaxSum([1, undefined, 3, 5, -3]));
-console.log(minMaxSum([1, NaN, 3, 5, -3]));
+minMaxSum([3, 0, -5, 1, 44, -12, 3, 0, 0, 1, 2, -3, -3, 2, 1, 4, -2 - 3 - 1]);
+minMaxSum([-1, -8, -2]);
+minMaxSum([1, 7, 3]);
+minMaxSum([1, undefined, 3, 5, -3]);
+minMaxSum([1, NaN, 3, 5, -3]);
 
 
 // https://docs.google.com/spreadsheets/d/1CC5nWd3HgY0cBi_1fw0hvrhyintW1X6O42XMVDUr5v8/edit?usp=sharing
@@ -79,20 +72,24 @@ console.log(minMaxSum([1, NaN, 3, 5, -3]));
 // // [2, 2, 1, 2, 2, 3, 0, 1, 2] // 4
 // // [2, 1, 5, 0, 3, 4, 7, 2, 3, 1, 8] // 24
 // // [2, 2, 2, 2, 2] // 0
-function findVolumeWater(mountains){
-    let volume = 0;
-    findMountMaxIndex = (arr) => Math.max.apply(null, arr);
+function findVolumeWater(mountains) {
+    let iMountMax = mountains.indexOf(Math.max.apply(null, mountains)),
+        volume = 0;
 
-    
+    sumVolume(mountains.slice(0, iMountMax + 1));
+    sumVolume(mountains.slice(iMountMax).reverse());
 
+    function sumVolume(mountains) {
+        let nextMaxMountain = 0;
+        mountains.forEach(elem => {
+            nextMaxMountain >= elem ?
+                volume += nextMaxMountain - elem :
+                nextMaxMountain = elem;
+        })
+    }
 
-
-
-
-
-
+    return volume;
 }
 
-
-console.log(`\nзадачка`);
-console.log(findVolumeWater([2, 1, 5, 0, 3, 4, 7, 2, 3, 1, 0]));
+findVolumeWater([2, 1, 5, 0, 3, 4, 7, 2, 3, 1, 0]); // 10
+findVolumeWater([2, 2, 2, 2, 2]); // 0
