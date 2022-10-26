4a6bdce5108be890025f6f6fadd958bd3e9ca1cf
Neschadin <abudarovich@gmail.com>
2022-10-26 22:15:45 +0300

    update HW1 & HW2

--- home_work_1_part_1/script.js
+++ home_work_1_part_1/script.js
@@ -1,34 +1,44 @@
 // Numbers
 // Отримати число pi з Math і округлити його до 2 знаків після крапки
-console.log(Math.PI.toFixed(2));
+let getPi = () => +Math.PI.toFixed(2);
+getPi();
 
 // Перевірити результат обчислення 0.6 + 0.7 – як привести до нормального вигляду (1.3)?
-console.log((0.6 + 0.7).toFixed(1));
+let sum = (a, b) => (a + b).toFixed(1);
+sum(0.6, 0.7);
 
 // Отримати число з рядка ‘100$’
-console.log(parseInt(`100$`));
+let getNumber = (str) => parseInt(str);
+getNumber('100$');
 
 // Strings:
-const str = `some test string`;
+const str = 'some test string';
 
 // Отримати першу й останню букви стрінги.
-console.log(str[0] + str[str.length - 1]);
+let getFirstLastLetter = (str) => str[0] + str.at(-1);
+getFirstLastLetter(str);
 
 // Зробити першу й останню букву великими
-console.log(
-  str.replace(/^.|.$/g, u => u.toUpperCase())
-);
+let setFirstLastLetter = (str) => str.replace(/^.|.$/g, (u) => u.toUpperCase());
+setFirstLastLetter(str);
 
 // Знайти позицію другого пробілу
-console.log(str.lastIndexOf(` `) + 1);
+let findSecondSpacePosition = (str) => {
+    let counter = 0,
+        index = 0;
+        str.split("").forEach((v, i) => {
+            if (v == " " && ++counter == 2) index = i
+        });
+    return counter < 2
+        ? "this string does not contain the required number of spaces"
+        : index;
+}
 
 // Отримати нову стрінгу без останніх 6 символів
-console.log(str.slice(0, str.length - 6));
+let getNewString = (str) => str.slice(0, str.length - 6);
+getNewString(str);
 
 // В першому підʼїзді квартири 1 -20, в другому 21 - 40, в третьому 41-60.
 // Створити функцію, в яку можна передати номер квартири і при виклику якої буде повертатись номер підʼїзда.
-searchEntryNumber = appNumber => Math.ceil(appNumber / 20);
-
-console.log(searchEntryNumber(1));
-console.log(searchEntryNumber(25));
-console.log(searchEntryNumber(60));
+let searchEntryNumber = appNumber => isFinite(appNumber) ? Math.ceil(appNumber / 20) : "apartment number was expected";
+searchEntryNumber(55);
