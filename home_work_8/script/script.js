3b052e08eca5cbf7f67a2f13dc496b9ab8e3b2b5
Neschadin Oleksandr <73835274+Neschadin@users.noreply.github.com>
2022-12-21 15:27:59 +0200

    upd HW8

--- home_work_8/script/script.js
+++ home_work_8/script/script.js
@@ -1,96 +1,23 @@
-const firstNameInput = document.getElementById("first_name");
-const lastNameInput = document.getElementById("last_name");
-firstNameInput.addEventListener("keyup", (e) => setValidState(e.target))
-lastNameInput.addEventListener("keyup", (e) => setValidState(e.target));
-
-const phoneNumberInput = document.getElementById("phone_number");
-phoneNumberInput.addEventListener("blur", (e) => setValidState(e.target));
-// User, for example can write 38067… or 067…. Both variants are valid, but on submit must record in format 38067…
-
-
-const emailInput = document.getElementById("email");
-// Email:
-// must be latin letters only. Big or small letters valid.
-// @foo.bar - not valid
-// foo@b.ar.baz - not valid (two dots after @)
-// foo.bar.@baz.com - not valid (dot before @)
-// 123@foo.bar - not valid (first part must include letters)
-// foo@123.123 - not valid (the same as previous)
-// foo@bar.baz, FOO123@bar.baz, foo.bar.baz@test.com  -  valid
-
-
-const passwordInput = document.getElementById("password");
-// Password:
-// Must have one or more digits
-// Must have one or more small and big letters
-// Length 8+ symbols
-// Must include one or more special characters (!, %, ?, #, $, * or others)
-// Confirm password - must be the same as password
-
-
-function validator(elem) {
-  if (elem.id === "first_name" || elem.id === "last_name") {
-    return /^[A-Z][a-z]+$/.test(elem.value);
-  }
-
-  if (elem.id === "phone_number") {
-    return ["[679]3", "6[78]", "9[678]", "50", "66", "39"].some(template => {
-      const reg = new RegExp("^(38)?0" + template + "\\d\\d\\d\\d\\d\\d\\d");
-
-      return !reg.test(elem.value) ? false :
-        /^38/.test(elem.value) ? true : elem.value = "38" + elem.value;
-    });
-  }
-
-  if (elem.id === "email") {
-
-    //  console.log(/^0[]$/.test(elem.value));
-  }
-}
-
-
-function setValidState(elem) {
-  validator(elem) ? elem.classList.remove("invalid") : elem.classList.add("invalid");
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 const root = document.documentElement;
-const eye = document.getElementById("eyeball");
-const beam = document.getElementById("beam");
+const passwordInput = document.getElementById("password");
+
+document.getElementById("first_name")
+  .addEventListener("keyup", (e) => setValidateState(e.target));
+
+document.getElementById("last_name")
+  .addEventListener("keyup", (e) => setValidateState(e.target));
+
+
+document.getElementById("phone_number")
+  .addEventListener("blur", (e) => setValidateState(e.target));
+
+document.getElementById("email")
+  .addEventListener("blur", (e) => setValidateState(e.target));
+
+passwordInput.addEventListener("keyup", (e) => setValidateState(e.target));
 
 root.addEventListener("mousemove", (e) => {
-  let rect = beam.getBoundingClientRect();
+  let rect = document.getElementById("beam").getBoundingClientRect();
   let mouseX = rect.right + rect.width / 2;
   let mouseY = rect.top + rect.height / 2;
   let rad = Math.atan2(mouseX - e.pageX, mouseY - e.pageY);
@@ -99,9 +26,55 @@ root.addEventListener("mousemove", (e) => {
   root.style.setProperty("--beamDegrees", `${degrees}deg`);
 });
 
-eye.addEventListener("click", (e) => {
+document.getElementById("eyeball")
+  .addEventListener("click", (e) => {
+    e.preventDefault();
+
+    document.body.classList.toggle("show-password");
+    passwordInput.type = passwordInput.type === "password" ? "text" : "password";
+    passwordInput.focus();
+  });
+
+document.querySelector("form").addEventListener("submit", (e) => {
   e.preventDefault();
-  document.body.classList.toggle("show-password");
-  passwordInput.type = passwordInput.type === "password" ? "text" : "password";
-  passwordInput.focus();
+  
+  Array.from(document.querySelectorAll("input"))
+    .every((item) => item.value && validator(item))
+    ? alert("All values are valid!") : alert("Wipe your eyes :)");
 });
+
+
+function validator(elem) {
+  const value = elem.value;
+
+  switch (elem.id) {
+    case "first_name": // Check: Latin letters only. First letter in uppercase. Length 2+ letters.
+    case "last_name":
+      return /^[A-Z][a-z]+$/.test(value);
+
+    case "phone_number": // Check: All Ukraine operators. Length are valid for phone numbers. Digits only. 38067… or 067… variants are valid, but will be returned 38067…
+      return ["[679]3", "6[78]", "9[678]", "50", "66", "39"].some(
+        (template) => {
+          const reg = new RegExp("^(38)?0" + template + "\\d\\d\\d\\d\\d\\d\\d");
+
+          return reg.test(value) && /^38/.test(value) ?
+            true : (elem.value = "38" + value);
+        }
+      );
+
+    case "email": // Check: Latin letters only. foo@bar.baz, F3@bar.baz, fo.bar.b@test.com - only such templates are valid.
+      return /^([a-z0-9]+(\.[a-z0-9]+)*)\@[a-z0-9]+\.[a-z]{2,}$/i.test(value);
+
+    case "password": // Check: minimum one digit, one small and big letters. Must include minimum one special symbol. Length 8+.
+      return /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[-#!$@%^&*_+~=:;?\/])[-\w#!$@%^&*+~=:;?\/]{8,}$/
+        .test(value);
+
+    default:
+      return false;
+  }
+}
+
+
+function setValidateState(elem) {
+  (!elem.value || validator(elem)) ? elem.classList.remove("invalid"): elem.classList.add("invalid");
+}
