1c21bc4d2c6e531425d5de1cd1dff109dc3acd10
Neschadin Oleksandr <73835274+Neschadin@users.noreply.github.com>
2022-12-21 16:45:06 +0200

    upd HW8

--- home_work_8/script/script.js
+++ home_work_8/script/script.js
@@ -37,10 +37,16 @@ document.getElementById("eyeball")
 
 document.querySelector("form").addEventListener("submit", (e) => {
   e.preventDefault();
-  
-  Array.from(document.querySelectorAll("input"))
-    .every((item) => item.value && validator(item))
-    ? alert("All values are valid!") : alert("Wipe your eyes :)");
+
+  const arr = Array.from(e.target.querySelectorAll("input"));
+  const checking = arr.every((item) => item.value && validator(item));
+
+  if (checking) {
+    arr.forEach(item => console.log(item))
+    e.target.innerHTML = "<h1>All values are valid!</h1>";
+  } else {
+    alert("Wipe your eyes :)");
+  }
 });
 
 
@@ -48,11 +54,11 @@ function validator(elem) {
   const value = elem.value;
 
   switch (elem.id) {
-    case "first_name": // Check: Latin letters only. First letter in uppercase. Length 2+ letters.
+    case "first_name": // Checking: Latin letters only. First letter in uppercase. Length 2+ letters.
     case "last_name":
       return /^[A-Z][a-z]+$/.test(value);
 
-    case "phone_number": // Check: All Ukraine operators. Length are valid for phone numbers. Digits only. 38067… or 067… variants are valid, but will be returned 38067…
+    case "phone_number": // Checking: All Ukraine operators. Length are valid for phone numbers. Digits only. 38067… or 067… variants are valid, but will be returned 38067…
       return ["[679]3", "6[78]", "9[678]", "50", "66", "39"].some(
         (template) => {
           const reg = new RegExp("^(38)?0" + template + "\\d\\d\\d\\d\\d\\d\\d");
@@ -62,10 +68,10 @@ function validator(elem) {
         }
       );
 
-    case "email": // Check: Latin letters only. foo@bar.baz, F3@bar.baz, fo.bar.b@test.com - only such templates are valid.
+    case "email": // Checking: Latin letters only. foo@bar.baz, F3@bar.baz, fo.bar.b@test.com - only such templates are valid.
       return /^([a-z0-9]+(\.[a-z0-9]+)*)\@[a-z0-9]+\.[a-z]{2,}$/i.test(value);
 
-    case "password": // Check: minimum one digit, one small and big letters. Must include minimum one special symbol. Length 8+.
+    case "password": // Checking: minimum one digit, one small and big letters. Must include minimum one special symbol. Length 8+.
       return /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[-#!$@%^&*_+~=:;?\/])[-\w#!$@%^&*+~=:;?\/]{8,}$/
         .test(value);
 
